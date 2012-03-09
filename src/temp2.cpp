9f0e3c0f9e86fce82a04ea112bf2c45083c2c1ae
udinev <forrts@mail.ru>
2012-03-09 15:09:52 +0700

    sAS

---
+++ src/temp2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : temp2.cpp
+// Author      : Yudin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
